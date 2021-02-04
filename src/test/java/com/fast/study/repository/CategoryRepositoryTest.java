641971f78befdabd28c239e6fc3d97ec82864629
git config --global user.name jeong-subingit config --global user.email subin930507@gmail.com <subin930507@gmail.com>
2021-02-04 12:10:54 +0900

    3 commit

---
+++ src/test/java/com/fast/study/repository/CategoryRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.fast.study.repository;
+
+import com.fast.study.StudyApplicationTests;
+import com.fast.study.model.entity.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class CategoryRepositoryTest  extends StudyApplicationTests {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void create(){
+        String type="COMPUTER";
+        String title="컴퓨터";
+        LocalDateTime createAt =LocalDateTime.now();
+        String createBy = "AdminServer";
+
+        Category category = new Category();
+        category.setType(type);
+        category.setTitle(title);
+        category.setCreatedAt(createAt);
+        category.setCreatedBy(createBy);
+
+        Category newCategory = categoryRepository.save(category);
+        Assertions.assertNotNull(newCategory);
+        Assertions.assertEquals(newCategory.getType(),type);
+        Assertions.assertEquals(newCategory.getTitle(),title);
+
+
+    }
+
+    @Test
+    public void read(){
+        String type="COMPUTER";
+        Optional<Category> optionalCategory = categoryRepository.findByType(type);
+        //select * from category where type='computer'
+
+        optionalCategory.ifPresent(c ->{
+            Assertions.assertEquals(c.getType(),type);
+            System.out.println(c.getId());
+            System.out.println(c.getType());
+            System.out.println(c.getTitle());
+        });
+    }
+}
